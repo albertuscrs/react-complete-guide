f38903adbe25de335f5ccf94f05664cc94710524
albertuscrs <achristyadhi@gmail.com>
2019-10-24 15:58:46 +0700

    Update: using jsx expression to render things

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classes from './Person.css'
+import Aux from '../../../hoc/Auxiliary'
 
 class Person extends Component {
 
   render (){
     console.log('[Person.js] rendering...');
-
-    return [
-        <p key='s' onClick={ this.props.click }>I&apos;m { this.props.name } and I am { this.props.age } years old!</p>,
-        < p key = 'sa'>{ this.props.children }</p>,
-        < input key = 'sz' type="text" onChange={ this.props.changed } value={ this.props.name }/>
-    ];
+    return (
+      <Aux>
+        <p onClick={ this.props.click }>I&apos;m { this.props.name } and I am { this.props.age } years old!</p>
+        <p>{ this.props.children }</p>
+        < input type="text" onChange={ this.props.changed } value={ this.props.name }/>
+      </Aux>
+    );
   }
 }
 
