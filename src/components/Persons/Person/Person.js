50a437a44c0256b34afa2978bd4db633e4fb52db
albertuscrs <achristyadhi@gmail.com>
2019-10-24 15:50:40 +0700

    Update: change the wrapping with one route to wrapping like an array actually it is an array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,15 +7,11 @@ class Person extends Component {
   render (){
     console.log('[Person.js] rendering...');
 
-    return (
-      < div className = {
-        classes.Person
-      } >
-        <p onClick={ this.props.click }>I&apos;m { this.props.name } and I am { this.props.age } years old!</p>
-        {/* <p>{ this.props.children }</p> */}
-        <input type="text" onChange={ this.props.changed } value={ this.props.name } />
-      </div>
-    )
+    return [
+        <p key='s' onClick={ this.props.click }>I&apos;m { this.props.name } and I am { this.props.age } years old!</p>,
+        < p key = 'sa'>{ this.props.children }</p>,
+        < input key = 'sz' type="text" onChange={ this.props.changed } value={ this.props.name }/>
+    ];
   }
 }
 
@@ -23,7 +19,8 @@ Person.propTypes = {
   click: PropTypes.func,
   name: PropTypes.string,
   age: PropTypes.number,
-  changed: PropTypes.func
+  changed: PropTypes.func,
+  children: PropTypes.string
 }
 
 export default Person;
