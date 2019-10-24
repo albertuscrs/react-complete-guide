b6935a791c32f76b0310cf3ce281f4ae746d888a
albertuscrs <achristyadhi@gmail.com>
2019-10-24 15:38:40 +0700

    Update: change component to pure component on persons because persons component need to check all props

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Person from './Person/Person'
 
-class Persons extends Component {
+class Persons extends PureComponent { // punya sendiri caranya untuk cek semua components yang ganti
   // static getDerivedStateFromProps(props, state){
   //   console.log('[Persons.js] getDerivedStateFromProps');
   //   return state;
   // }
 
-  shouldComponentUpdate(nextProps, nextState){
-    console.log('[Persons.js] shouldComponentUpdate');
-    if(nextProps.persons !== this.props.persons){
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
+  // shouldComponentUpdate(nextProps, nextState){
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   if(nextProps.persons !== this.props.persons ||
+  //      nextProps.changed !== this.props.changed ||
+  //      nextProps.clicked !== this.props.clicked){
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }
+  // }
 
   getSnapshotBeforeUpdate(prevProps, prevState){
     console.log('[Persons.js] getSnapshotBeforeUpdate');
