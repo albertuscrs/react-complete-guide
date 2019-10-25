edef0ff6534b3c2593b1fb2a9ff5278d27adc6d8
albertuscrs <achristyadhi@gmail.com>
2019-10-25 16:32:34 +0700

    Learning: how to use ref in modern way

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,8 +5,15 @@ import Aux from '../../../hoc/Auxiliary'
 import withClass from '../../../hoc/withClass'
 
 class Person extends Component {
+
+  constructor(props){
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.inputElement.focus();
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
   }
 
   render (){
@@ -17,7 +24,8 @@ class Person extends Component {
         <p>{ this.props.children }</p>
         < input
         type="text"
-        ref={(inputEl) => {this.inputElement = inputEl}}
+        // ref={(inputEl) => {this.inputElement = inputEl}}
+        ref={this.inputElementRef}
         onChange={ this.props.changed }
         value={ this.props.name }/>
       </Aux>
